b671b5273082289373ae411f90493465a938fa96
JULIO ESPINOSA MARTINEZ <jumar.789@gmail.com>
2021-11-10 21:48:58 -0500

    Add files via upload

---
+++ Models/ProductosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Productos
+{
+    public class ProductosModel
+    {
+            public string _id { get; set; }
+            public string nombre { get; set; }
+            public string categoria { get; set; }
+            public string presentacion { get; set; }
+            public string precio { get; set; }
+            public string tiempo { get; set; }
+    
+    }
+}
